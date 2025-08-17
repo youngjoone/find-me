f2e3c2d3ce39f27412571bcc4f97a66e5b94e59f
yonngjoone <youngjoone@gmail.com>
2025-08-17 21:04:03 +0900

    feat(auth): Implement all auth service features as per LOGIN.md (local dev)

--- auth-service/src/main/java/com/vibe/auth/common/exception/InvalidCredentialsException.java
+++ auth-service/src/main/java/com/vibe/auth/common/exception/InvalidCredentialsException.java
@@ -1,10 +1,9 @@
-
 package com.vibe.auth.common.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
+@Status(HttpStatus.UNAUTHORIZED)
 public class InvalidCredentialsException extends RuntimeException {
     public InvalidCredentialsException(String message) {
         super(message);
